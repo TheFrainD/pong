02890944f7f384ec5dd2aaaeff5e7720122939c3
Dmytro Zykov <dmytri.zykov@gmail.com>
2024-09-14 21:59:06 +0300

    Add multiple scripts per entity support

--- include/comp/script.h
+++ include/comp/script.h
@@ -1,20 +1,21 @@
 #pragma once
 
+#include <entt/entity/registry.hpp>
 #include <filesystem>
 #include <sol/object.hpp>
+#include <string>
 #include <unordered_map>
 
 #include "sys/scripts.h"
 
 namespace pong::comp {
 
-struct Script {
-  explicit Script(
-      sys::ScriptSystem &script_system, const std::filesystem::path &path,
-      const std::unordered_map<std::string, sol::object> &params = {})
-      : id(script_system.RegisterScript(path, params)) {}
-
-  int id;
+struct ScriptComponent {
+  std::unordered_map<std::string, int> scripts;
 };
+
+void AddScript(entt::registry &registry, sys::ScriptSystem &script_system,
+               entt::entity entity, const std::filesystem::path &path,
+               const std::unordered_map<std::string, sol::object> &params = {});
 
 }  // namespace pong::comp
