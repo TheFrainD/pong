8b8b3659667e721d80fc95373ed4f65db469359b
Dmytro Zykov <dmytri.zykov@gmail.com>
2024-09-25 21:12:43 +0300

    Modernize test code

--- src/core/test/collision_test.cpp
+++ src/core/test/collision_test.cpp
@@ -3,6 +3,7 @@
 #include <core/sys/physics.h>
 #include <gtest/gtest.h>
 
+#include <algorithm>
 #include <entt/entity/registry.hpp>
 #include <entt/signal/dispatcher.hpp>
 #include <vector>
@@ -10,83 +11,80 @@
 class CollisionTest : public testing::Test {
  protected:
   CollisionTest() {
-    dispatcher.sink<core::comp::CollisionEvent>()
+    dispatcher_.sink<core::comp::CollisionEvent>()
         .connect<&CollisionTest::CollisionEvent>(this);
   }
 
   void CollisionEvent(const core::comp::CollisionEvent &event) {
-    events.push_back(event);
+    events_.push_back(event);
   }
 
   bool HasCollision(entt::entity a, entt::entity b) {
-    for (const auto &event : events) {
-      if ((event.a == a && event.b == b) || (event.a == b && event.b == a)) {
-        return true;
-      }
-    }
-    return false;
+    return std::ranges::any_of(events_, [a, b](const auto &event) {
+      return (event.a == a && event.b == b) || (event.a == b && event.b == a);
+    });
   }
 
-  std::vector<core::comp::CollisionEvent> events;
+  std::vector<core::comp::CollisionEvent> events_;
 
-  entt::registry registry;
-  entt::dispatcher dispatcher;
+  entt::registry registry_;
+  entt::dispatcher dispatcher_;
 };
 
 TEST_F(CollisionTest, Collision) {
-  auto e1 = registry.create();
-  registry.emplace<core::comp::Transform>(e1, Vector2{2.0f, 1.0f});
-  registry.emplace<core::comp::Collider>(e1, Vector2{3.0f, 2.0f});
+  auto e1 = registry_.create();
+  registry_.emplace<core::comp::Transform>(e1, Vector2{2.0F, 1.0F});
+  registry_.emplace<core::comp::Collider>(e1, Vector2{3.0F, 2.0F});
 
-  auto e2 = registry.create();
-  registry.emplace<core::comp::Transform>(e2, Vector2{4.0f, 2.0f});
-  registry.emplace<core::comp::Collider>(e2, Vector2{2.0f, 2.0f});
+  auto e2 = registry_.create();
+  registry_.emplace<core::comp::Transform>(e2, Vector2{4.0F, 2.0F});
+  registry_.emplace<core::comp::Collider>(e2, Vector2{2.0F, 2.0F});
 
-  core::sys::PhysicsUpdate(registry, dispatcher);
+  core::sys::PhysicsUpdate(registry_, dispatcher_);
 
   ASSERT_TRUE(HasCollision(e1, e2));
 }
 
 TEST_F(CollisionTest, NoCollision) {
-  auto e1 = registry.create();
-  registry.emplace<core::comp::Transform>(e1, Vector2{2.0f, 1.0f});
-  registry.emplace<core::comp::Collider>(e1, Vector2{3.0f, 2.0f});
+  auto e1 = registry_.create();
+  registry_.emplace<core::comp::Transform>(e1, Vector2{2.0F, 1.0F});
+  registry_.emplace<core::comp::Collider>(e1, Vector2{3.0F, 2.0F});
 
-  auto e2 = registry.create();
-  registry.emplace<core::comp::Transform>(e2, Vector2{5.0f, 2.0f});
-  registry.emplace<core::comp::Collider>(e2, Vector2{2.0f, 2.0f});
+  auto e2 = registry_.create();
+  registry_.emplace<core::comp::Transform>(e2, Vector2{5.0F, 2.0F});
+  registry_.emplace<core::comp::Collider>(e2, Vector2{2.0F, 2.0F});
 
-  core::sys::PhysicsUpdate(registry, dispatcher);
+  core::sys::PhysicsUpdate(registry_, dispatcher_);
 
   ASSERT_FALSE(HasCollision(e1, e2));
 }
 
 TEST_F(CollisionTest, OffsetCollision) {
-  auto e1 = registry.create();
-  registry.emplace<core::comp::Transform>(e1, Vector2{2.0f, 1.0f});
-  registry.emplace<core::comp::Collider>(e1, Vector2{3.0f, 2.0f});
+  auto e1 = registry_.create();
+  registry_.emplace<core::comp::Transform>(e1, Vector2{2.0F, 1.0F});
+  registry_.emplace<core::comp::Collider>(e1, Vector2{3.0F, 2.0F});
 
-  auto e2 = registry.create();
-  registry.emplace<core::comp::Transform>(e2, Vector2{5.0f, 2.0f});
-  registry.emplace<core::comp::Collider>(e2, Vector2{2.0f, 2.0f},
-                                         Vector2{-1.0f, 0.0f});
+  auto e2 = registry_.create();
+  registry_.emplace<core::comp::Transform>(e2, Vector2{5.0F, 2.0F});
+  registry_.emplace<core::comp::Collider>(e2, Vector2{2.0F, 2.0F},
+                                          Vector2{-1.0F, 0.0F});
 
-  core::sys::PhysicsUpdate(registry, dispatcher);
+  core::sys::PhysicsUpdate(registry_, dispatcher_);
 
   ASSERT_TRUE(HasCollision(e1, e2));
 }
 
 TEST_F(CollisionTest, OffsetNoCollision) {
-  auto e1 = registry.create();
-  registry.emplace<core::comp::Transform>(e1, Vector2{2.0f, 1.0f});
-  registry.emplace<core::comp::Collider>(e1, Vector2{3.0f, 2.0f});
+  auto e1 = registry_.create();
+  registry_.emplace<core::comp::Transform>(e1, Vector2{2.0F, 1.0F});
+  registry_.emplace<core::comp::Collider>(e1, Vector2{3.0F, 2.0F});
 
-  auto e2 = registry.create();
-  registry.emplace<core::comp::Transform>(e2, Vector2{4.0f, 2.0f});
-  registry.emplace<core::comp::Collider>(e2, Vector2{2.0f, 2.0f},
-                                         Vector2{1.0f, 0.0f});
+  auto e2 = registry_.create();
+  registry_.emplace<core::comp::Transform>(e2, Vector2{4.0F, 2.0F});
+  registry_.emplace<core::comp::Collider>(e2, Vector2{2.0F, 2.0F},
+                                          Vector2{1.0F, 0.0F});
 
-  core::sys::PhysicsUpdate(registry, dispatcher);
+  core::sys::PhysicsUpdate(registry_, dispatcher_);
 
   ASSERT_FALSE(HasCollision(e1, e2));
 }
